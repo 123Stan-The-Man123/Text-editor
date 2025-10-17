88be7f8706faf7de61bdfb7d2e8eda5d73ea71ad
Stanislaw Molsa <Molsas2020@gmail.com>
2025-10-17 19:50:41 +0100

    Split main into smaller subroutines. Fixed an issue with handling ESC. Added Line and Node structs for the doubly-linked list, which will be used as the text buffer.

--- src/main.c
+++ src/main.c
@@ -9,7 +9,7 @@
 #define CURSOR_RIGHT "\033[1C"
 #define CURSOR_LEFT "\033[1D"
 
-// Erasures
+// Erasure codes
 #define ERASE_LINE "\033[2K"
 #define ERASE_SCREEN "\033[2J"
 
@@ -19,15 +19,24 @@
 #define ESC 27
 #define BACK_SPACE 127
 
-// Handle arrow keys
-void handle_arrows(void);
+struct Line {
+    char* buff;
+    int len;
+};
+
+struct Node {
+    struct Node* prev;
+    struct Line buffer;
+    struct Node* next;
+};
+
+int process_input(void);
+void process_escape(void);
+int enable_raw_mode(struct termios* orig_term, struct termios* raw);
+void disable_raw_mode(struct termios* orig_term);
 
 int main(void)
 {
-    // Turns off the STDOUT buffer
-    setbuf(stdout, NULL);
-    printf(RESET_CURSOR ERASE_SCREEN);
-    
     /*
     struct termios {
         tcflag_t        c_iflag;        input flags
@@ -39,56 +48,53 @@ int main(void)
         speed_t         c_ospeed;       output speed
     };
     */
-
     struct termios orig_term, raw;
-
-    // Return an error if tcgetattr fails
-    if (tcgetattr(STDIN_FILENO, &orig_term) != 0) {
-        perror("tcgetattr");
+    if (enable_raw_mode(&orig_term, &raw) == -1)
         return -1;
-    }
 
-    // Make a separate copy so the original settings are saved
-    raw = orig_term;
-    
-    // Enter raw mode
-    cfmakeraw(&raw);
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
-    
-    // Event loop
-    for (int running = 1; running;) {
-        char c = getchar();
-        switch (c) {
-            case CTRL_Q:
-                running = 0;
-                break;
-            case CTRL_K:
-                printf(ERASE_LINE);
-                break;
-            case '\r':
-                printf("\r\n");
-                break;
-            case ESC:
-                handle_arrows();
-                break;
-            case BACK_SPACE:
-                printf("\b \b");
-                break;
-            default:
-                printf("%c", c);
-                break;
-        }
-    }
-    
-    // Reset terminal to default settings
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_term);
+    for (int running = 1; running;)
+        running = process_input();
+
+    disable_raw_mode(&orig_term);
+
     return 0;
 }
 
-void handle_arrows(void)
+int process_input(void)
 {
-    getchar();
-    switch (getchar()) {
+    char c = getc(stdin);
+    switch (c) {
+        case CTRL_Q:
+            return 0;
+        case CTRL_K:
+            printf(ERASE_LINE);
+            break;
+        case '\r':
+            printf("\r\n");
+            break;
+        case ESC:
+            process_escape();
+            break;
+        case BACK_SPACE:
+            printf("\b \b");
+            break;
+        default:
+            printf("%c", c);
+            break;
+    }
+
+    return 1;
+}
+
+void process_escape(void)
+{
+    char c = getc(stdin);
+    if (c != '[') {
+        ungetc(c, stdin);
+        return ;
+    }
+
+    switch (getc(stdin)) {
         case 'A':
             printf(CURSOR_UP);
             break;
@@ -102,4 +108,32 @@ void handle_arrows(void)
             printf(CURSOR_LEFT);
             break;
     }
+}
+
+int enable_raw_mode(struct termios* orig_term, struct termios* raw)
+{
+    // Turns off the STDOUT buffer
+    setbuf(stdout, NULL);
+    printf(RESET_CURSOR ERASE_SCREEN);
+
+    // Return an error if tcgetattr fails
+    if (tcgetattr(STDIN_FILENO, orig_term) != 0) {
+        perror("tcgetattr");
+        return -1;
+    }
+    // Make a separate copy so the original settings are saved
+    *raw = *orig_term;
+    
+    // Enter raw mode
+    cfmakeraw(raw);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, raw);
+
+    return 0;
+}
+
+void disable_raw_mode(struct termios* orig_term)
+{
+    // Reset terminal to default settings
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, orig_term);
+    printf(ERASE_SCREEN RESET_CURSOR);
 }
